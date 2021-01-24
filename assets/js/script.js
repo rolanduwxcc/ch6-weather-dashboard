505a54bdd4907ac7343fc3cddee4664d2d554f4a
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-23 22:27:38 -0600

    updated dates and weather icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 //----------------------------------------------GLOBAL VARIABLES
-var openWeatherKey="eb3997fae2c925d2df0bffe2f227aa9d";
-var savedCities = [];
-var currentCity = "Madison,WI,US";
+let openWeatherKey="eb3997fae2c925d2df0bffe2f227aa9d";
+let savedCities = [];
+let currentCity="Madison,WI,US";
+let cityInputEl = document.getElementById('city');
+let citySearchFormEl = document.getElementById('city-search-form');
+let currentWeatherEl = document.getElementById('current-weather');
 
-var currentWeatherEl = document.getElementById('current-weather');
 
 //----------------------------------------------FUNCTIONS
 function fetchWeatherFor(city) {
@@ -11,15 +13,14 @@ function fetchWeatherFor(city) {
     var longitude;
     var geoCodeLimits=5;
     var openWeatherGeoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid="+openWeatherKey+"&limit="+geoCodeLimits;
-    //PARAMS TO ADD{city name},{state code},{country code}&limit={limit}&appid={API key}
-    console.log(openWeatherGeoCodeURL);
+    //see https://openweathermap.org/api/geocoding-api#direct_how for reference on how to configure this URL.
 
     fetch(openWeatherGeoCodeURL).then(function(response) {
         if(response.ok) {
             response.json().then(function(coordinatesJSON){
                 latitude=coordinatesJSON[0].lat;
                 longitude=coordinatesJSON[0].lon;
-                console.log(latitude + "," +longitude);
+                //feed the coordinates back to openweathermap to get all the date we need
                 fetchWeatherForCoordinates(latitude,longitude);
             });
         } else {
@@ -35,14 +36,12 @@ function fetchWeatherForCoordinates(latitude,longitude) {
     var weatherUnits="imperial";
     var weatherDataToExclude="minutely,hourly,alerts";
     var openWeatherUrl ="https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude="+weatherDataToExclude+"&appid="+openWeatherKey+"&units="+weatherUnits;
-    //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-    // var openWeatherUrl="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+openWeatherKey+"&units="+weatherUnits;
-    console.log(openWeatherUrl);
+    //see https://openweathermap.org/api/one-call-api for reference on how to configure this URL.
     
     fetch(openWeatherUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(weatherJSON) {
-                console.log(weatherJSON);
+                //take the weather data we got back and build HTML
                 buildCurrentWeatherHTML(weatherJSON);
                 buildFiveDayForecastHTML(weatherJSON);
             });
@@ -56,11 +55,10 @@ function fetchWeatherForCoordinates(latitude,longitude) {
 }
 
 function buildCurrentWeatherHTML(fromWeatherJSON) {
-    console.log(fromWeatherJSON);
-
+    currentWeatherEl.innerHTML = "";
     //Header = city (date) icon
     var headerEl = document.createElement("h3");
-    headerEl.innerHTML = currentCity + "(" + fromWeatherJSON.current.dt + ")" + fromWeatherJSON.current.weather[0].icon;
+    headerEl.innerHTML = currentCity + " (" + getReadableDate(fromWeatherJSON.current.dt) + ") " + "<img src='http://openweathermap.org/img/wn/"+fromWeatherJSON.current.weather[0].icon+"@2x.png'>";
     currentWeatherEl.appendChild(headerEl);
 
     //Temperature
@@ -85,26 +83,68 @@ function buildCurrentWeatherHTML(fromWeatherJSON) {
 }
 
 function buildFiveDayForecastHTML(fromWeatherJSON) {
-    var dailyWeatherArray = fromWeatherJSON.daily;
-    console.log(dailyWeatherArray);
+    let dailyWeatherArray = fromWeatherJSON.daily;
+    let dailyWeatherArrayLength = dailyWeatherArray.length;
+    //only care about at most next 5 days of weather
+    if (dailyWeatherArrayLength > 5) {
+        dailyWeatherArrayLength = 5; 
+    };
+    
     //loop over weatherJSON.daily 0
- 
-    //Date
+    for (let i = 0; i < dailyWeatherArrayLength; i++) {
+        const weatherForDay = dailyWeatherArray[i];
+               
+        let elementID = "day-" + i.toString();
+        let dayCardEl = document.createElement("div");
+        dayCardEl.setAttribute("class","card");
+        let dateEl = document.createElement("h4");
+        let iconEl = document.createElement("p");
+        let temperatureEl = document.createElement("p");
+        let humidityEl = document.createElement("p");        
 
-    //icon of weather
+        let date = getReadableDate(weatherForDay.dt);
+        dateEl.innerHTML = date;
+        dayCardEl.appendChild(dateEl);
 
-    //Temperature
+        iconEl.innerHTML = "<img src='http://openweathermap.org/img/wn/"+weatherForDay.weather[0].icon+"@2x.png'>";
+        dayCardEl.appendChild(iconEl);
 
-    //Humidity
+        temperatureEl.innerHTML = "Temperature: " + weatherForDay.temp.max;
+        dayCardEl.appendChild(temperatureEl);
+
+        humidityEl.innerHTML = "Humidity: " + weatherForDay.humidity;
+        dayCardEl.appendChild(humidityEl);
+
+        document.getElementById(elementID).innerHTML="";
+        document.getElementById(elementID).appendChild(dayCardEl);
+    }
+}
+
+function getReadableDate(openWeatherUnixDate) {
+    let date = new Date(openWeatherUnixDate*1000);
+    return (date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
+}
+
+function citySearchHandler(event) {
+    event.preventDefault();
+
+    //get the submited city value
+    currentCity = cityInputEl.value.trim();
+    console.log(currentCity);
+    if (currentCity) {
+        fetchWeatherFor(currentCity);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a valide city or city,state");
+    }
+    
 }
 
 
 
-
-
 //----------------------------------------------CALLS
-fetchWeatherFor(currentCity);
-
+// fetchWeatherFor(currentCity);
+citySearchFormEl.addEventListener("submit",citySearchHandler);
 
 
 
